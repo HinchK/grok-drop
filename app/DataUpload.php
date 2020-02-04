ff9865262b10a6bf7233f4fb26235b0fe44a2842
hinchk <kasey.hinchman@gmail.com>
2020-02-04 05:48:44 -0800

    add relations and controller

--- app/DataUpload.php
+++ app/DataUpload.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DataUpload extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'site',
+        'data',
+        'note'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
