adef4e28c2514a0d12857cd0b514e9bc0b4a8754
hinchk <kasey.hinchman@gmail.com>
2020-02-04 06:10:18 -0800

    store method in controller

--- app/Http/Controllers/DataUploadController.php
+++ app/Http/Controllers/DataUploadController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class DataUploadController extends Controller
 {
-    //
-}
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required:max:255',
+            'site' => 'required',
+            'note' => 'required:max:255'
+        ]);
+
+        auth()->user()->files()->create([
+            'title' => $request->get('title'),
+            'site' => $request->get('site'),
+            'note' => $request->get('note')
+        ]);
+
+        return back()->with('message', 'Your file is submitted Successfully');
+    }}
